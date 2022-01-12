a743455a613d7b1b556e774b9c20cc9033b2a8f8
Iryna Hudymenko <iryn4ik09@gmail.com>
2022-01-13 00:39:25 +0200

    321

--- src/index.js
+++ src/index.js
@@ -3,35 +3,29 @@ const DEBOUNCE_DELAY = 300;
 const userList = document.getElementById("search-box");
 const nameCountry = document.querySelector('.country-list');
 const aboutCountry = document.querySelector('.country-info');
-userList.addEventListener('input', (e) => {
-    // fetchCountries()
-    //     .then((countriesData) => initialize(countriesData))
-    // .catch((error) => console.log(error));
-    nameCountry.textContent = e.currentTarget.value;
-    
-})
-console.log(userList);
+userList.addEventListener('input', onSearch)
 
-// function fetchCountries(){
-fetch("https://restcountries.com/v2/all")
-    .then(function (res) {
-        return res.json();
-    })
-    .then(function (data) {
-        initialize(data);
-    })
-    .catch(function (err) {
-        console.log('Error', err);
-    })
-function initialize(countriesData) {
-    console.log(countriesData);
-        const options = countriesData.map((country) => {
-        return `<li class="list-item new">${country.name}</li>`;
-    })
-        .join("");
-    nameCountry.innerHTML = options;
-    console.log(options);
-              }
-   
 
+        
+function onSearch(e) {
+    userList.textContent = e.currentTarget.value;
+
+    fetchCountry(userList.textContent)
+        .then(initialize)
+    .catch(error => console.log(error));
+}
+
+function fetchCountry() {
+    return fetch('https://restcountries.com/v2/all')
+        .then(response => response.json())
+        .then(data => initialize(data))
+        .catch(error => console.log(error));
+}
+let countries
+function initialize(countryNames) {
+    const markup = countryNames.map((countryName) => {
+            return `<li>${countryName.name}</li>`
+        }).join('');
+    nameCountry.innerHTML = markup;
+}
    

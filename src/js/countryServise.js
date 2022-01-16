01a62701405461d9aeeecbaa5eb5a96025f807b9
Iryna Hudymenko <iryn4ik09@gmail.com>
2022-01-16 19:49:40 +0200

    done

--- src/js/countryServise.js
+++ src/js/countryServise.js
@@ -1,16 +1,7 @@
 'use strict';
-
-// const baseUrl = 'https://restcountries.com/v2/name/${name}';
-
 export default {
     fetchArticles(name) {
-    // // const requestParams = `${query}`;
-    //     const searchParams = 'name,capital,population,languages,flag'; `https://restcountries.com/v2/name/${name}?fields=${searchParams}`
-    // return fetch(baseUrl + searchParams).then(res => res.json());
-    // const languages = Object.values(languages);
-    const responseFields = 'name,capital,population,flags,languages';
+   const responseFields = 'name,capital,population,flags,languages';
    return fetch(`https://restcountries.com/v2/name/${name}?fields=${responseFields}`).then(res => res.json())
-   console.log(res.json()); 
-  },
+   },
 };
-// const searchParams = 'name,capital,population,flags,languages'; `https://restcountries.com/v3.1/name/${name}?fields=${searchParams}`
